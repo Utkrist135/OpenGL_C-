a6960bd205ceb75819a6e590343657b08c27d1e2
Utkrist Uprety <upretyutkrist@gmail.com>
2022-11-29 21:56:31 +0100

    Included GLEW as a loading library

--- OpenGL_Cherno/src/application.cpp
+++ OpenGL_Cherno/src/application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -8,16 +10,21 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        return -1; 
     }
      
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if(glewInit()!=GLEW_OK)
+        std:: cout <<"error";
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
