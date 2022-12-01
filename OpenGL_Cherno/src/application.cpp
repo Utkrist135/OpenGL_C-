74ebe93978ae9ba8ca36138e2aeea828202e1a04
Utkrist Uprety <upretyutkrist@gmail.com>
2022-12-01 16:28:14 +0100

    Implemented buffers to draw triangle

--- OpenGL_Cherno/src/application.cpp
+++ OpenGL_Cherno/src/application.cpp
@@ -16,15 +16,26 @@ int main(void)
     if (!window)
     {
         glfwTerminate();
-        return -1; 
+        return -1;
     }
-     
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    if(glewInit()!=GLEW_OK)
-        std:: cout <<"error";
+    if (glewInit() != GLEW_OK)
+        std::cout << "error";
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.5f, 0.5f,
+        0.5f, -0.5f };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -32,11 +43,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.5f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
 
 
         /* Swap front and back buffers */
